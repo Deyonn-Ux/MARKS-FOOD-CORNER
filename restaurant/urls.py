41fe97c4108752d64cc99663922b2c9f06343b47
Deyonn-Ux <alfdionbaumbad@gmail.com>
2026-05-29 20:51:51 +0800

    Fix media URL to use Cloudinary in production

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,4 +8,5 @@ urlpatterns = [
     path('', include('menu.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
